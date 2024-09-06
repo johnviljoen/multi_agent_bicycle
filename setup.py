9db25d0923cf854cb8b22817b6044bc0ad80d98f
johnviljoen <johnviljoen2@gmail.com>
2024-09-06 12:33:50 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='multi_agent_bicycle',
+    version='0.0.1',
+    author='John Viljoen',
+    author_email='johnviljoen2@gmail.com',
+    install_requires=[
+        'matplotlib',   # plotting...
+        'tqdm',         # just for pretty loops in a couple places
+        'numpy'
+    ],
+    packages=find_packages(include=[]),
+)
+
